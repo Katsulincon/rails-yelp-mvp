8b6fb97584f8a9662511a60e0b888824881ccdd6
Munekatsu Furugen <rinconlincon@gmail.com>
2020-05-13 15:44:37 +0900

    Embedded review form in the show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
+  RATES = (0..5).to_a
   belongs_to :restaurant
   validates :content, :rating, presence: true
   validates :rating, inclusion: { in: 0..5 }, numericality: true
